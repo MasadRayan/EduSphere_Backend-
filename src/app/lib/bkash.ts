b17835d6ed41715855de2228e54ce608afd37f6b
Masad Rayan <masadrayan2002@gmail.com>
2026-09-22 20:03:38 +0600

    refactor(bkash): clean up code formatting and improve error handling for token grants

--- src/app/lib/bkash.ts
+++ src/app/lib/bkash.ts
@@ -3,7 +3,6 @@ import { AppError } from "../utils/AppError";
 import { redisClient } from "./redis";
 import httpStatus from "http-status";
 
-
 export const getBkashIdToken = async () => {
 	try {
 		const IdTokenKey = "bkash:idToken";
@@ -48,7 +47,10 @@ export const getBkashIdToken = async () => {
 				},
 			);
 			if (!refreshTokenResponse.ok) {
-				throw new AppError(httpStatus.BAD_GATEWAY, "Bkash Access Token Grant Failed");
+				throw new AppError(
+					httpStatus.BAD_GATEWAY,
+					"Bkash Access Token Grant Failed",
+				);
 			}
 
 			const bkashRefreshTokenResult = await refreshTokenResponse.json();
@@ -87,7 +89,10 @@ export const getBkashIdToken = async () => {
 		);
 
 		if (!response.ok) {
-			throw new AppError(httpStatus.BAD_GATEWAY, "Bkash Access Token Grant Failed");
+			throw new AppError(
+				httpStatus.BAD_GATEWAY,
+				"Bkash Access Token Grant Failed",
+			);
 		}
 
 		const result = await response.json();
